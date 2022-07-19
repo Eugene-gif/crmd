3838b37f56549fbfc580600f3de5a12d4a39dfce
itpear <yurecbilnovgelarm@gmail.com>
2022-07-19 16:52:55 +0300

    правки перед авторизацией

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
     : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
-
+  
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
@@ -24,6 +24,7 @@ export default route(function (/* { store, ssrContext } */) {
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE)
+    
   })
 
   return Router
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,16 +1,53 @@
 
 const routes = [
   {
+    path: '/auth',
+    meta: {auth: false},
+    component: () => import('layouts/AuthLayout.vue'),
+    children: [
+      {
+        path: '/login',
+        component: () => import('pages/Auth/Login.vue')
+      },
+      {
+        path: '/register',
+        component: () => import('pages/Auth/Register.vue')
+      }
+    ]
+  },
+  {
     path: '/',
+    meta: {auth: true},
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') },
-      { path: '/finance', component: () => import('pages/Finance/index.vue') },
-      { path: '/projects', component: () => import('src/pages/Project/List.vue') },
-      { path: '/id', component: () => import('src/pages/Project/Id.vue') },
-      { path: '/contractor', component: () => import('src/pages/Contractor/Index.vue') },
-      { path: '/clients', component: () => import('src/pages/Сlients/Index.vue') },
-      { path: '/estimates', component: () => import('src/pages/Estimates/Index.vue') },
+      { 
+        path: '',
+        component: () => import('pages/Index.vue') 
+      },
+      { 
+        path: '/finance',
+        component: () => import('pages/Finance/index.vue') 
+      },
+      { 
+        path: '/projects',
+        component: () => import('src/pages/Project/List.vue') 
+      },
+      { 
+        path: '/id',
+        component: () => import('src/pages/Project/Id.vue') 
+      },
+      { 
+        path: '/contractor',
+        component: () => import('src/pages/Contractor/Index.vue') 
+      },
+      { 
+        path: '/clients',
+        component: () => import('src/pages/Сlients/Index.vue') 
+      },
+      { 
+        path: '/estimates',
+        component: () => import('src/pages/Estimates/Index.vue') 
+      },
     ]
   },
 
@@ -21,5 +58,7 @@ const routes = [
     component: () => import('pages/Error404.vue')
   }
 ]
+
+
 
 export default routes
