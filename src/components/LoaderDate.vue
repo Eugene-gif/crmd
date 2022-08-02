78708eea2ac73edaa995c0d26bfbfa48aacb0af5
itpear <yurecbilnovgelarm@gmail.com>
2022-08-02 23:58:40 +0300

    лоадер

---
+++ src/components/LoaderDate.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="loader">
+    <div class="loader-content">
+      <div class="circle">
+        <div class="item"></div>
+        <img src="~assets/loader.svg" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  setup() {
+    
+  },
+}
+</script>
