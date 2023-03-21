bcd986b0ac42c3e4fab48d9bcb55315009b5ca3a
itpear <yurecbilnovgelarm@gmail.com>
2023-03-21 03:12:09 +0300

    страница клиента профиль 2

--- src/components/Clients/Table.vue
+++ src/components/Clients/Table.vue
@@ -13,148 +13,146 @@
         @click.stop=""
       />
     </template>
-      <q-table
-        flat
-        :rows="rows2"
-        :columns="columns"
-        row-key="id"
-        hide-pagination
-        class="my-table projects-table projects-table-stripes"
-        v-for="table in 3"
-        :key="table"
-      >
-        <template #body="props">
-          <q-tr
+    <q-table
+      flat
+      :rows="rows2"
+      :columns="columns"
+      row-key="id"
+      hide-pagination
+      class="my-table projects-table projects-table-stripes"
+    >
+      <template #body="props">
+        <q-tr
+          :props="props"
+          class="q-tr-tiles"
+        >
+          <q-item to="id" class="q-tr-tiles__link" />
+          <q-td
+            key="image"
             :props="props"
-            class="q-tr-tiles"
+            class="q-td-image"
           >
-            <q-item to="id" class="q-tr-tiles__link" />
-            <q-td
-              key="image"
-              :props="props"
-              class="q-td-image"
-            >
-              <q-item to="/id">
-                <img v-show="props.row.image" :src="props.row.image" alt="">
+            <q-item to="/id">
+              <img v-show="props.row.image" :src="props.row.image" alt="">
+            </q-item>
+          </q-td>
+          <q-td
+            key="name"
+            :props="props"
+            class="q-td-name"
+          >
+            <div class="text-h3">
+              <span class="name-ico">{{props.row.iconName}}</span>{{props.row.name}}
+            </div>
+          </q-td>
+          <q-td
+            key="action"
+            :props="props"
+            class="q-td-action"
+          >
+            <ActionBtn 
+              :propsEl="props.row.id"
+              :offsetYX="[55, -258]"
+              :actionfunc="actionfunc"
+              @actionUpdate="onActionUpdate"
+              @actionCopy="onActionCopy"
+              @actionDel="onActionDel"
+            />
+          </q-td>
+          <q-td
+            key="square"
+            :props="props"
+            class="q-td-square"
+          >
+            <div class="text">Квартира, {{props.row.square}} м<sup>2</sup></div>
+          </q-td>
+          <q-td
+            key="changed"
+            :props="props"
+            class="q-td-changed"
+          >
+            <div class="text">Изменен: {{props.row.changed}}</div>
+          </q-td>
+          <q-td
+            key="customer"
+            :props="props"
+            class="q-td-customer"
+          >
+            <div class="text">{{props.row.customer}}</div>
+          </q-td>
+          <q-td
+            key="created"
+            :props="props"
+            class="q-td-created"
+          >
+            <div class="text">Создан: {{props.row.created}}</div>
+          </q-td>
+          <q-td
+            key="address"
+            :props="props"
+            class="q-td-address"
+          >
+            <div class="text">{{props.row.address}}</div>
+          </q-td>
+          <q-td style="flex: 0 0 100%;order: 4;" v-if="tab === 'stripes'"></q-td>
+          <q-td
+            key="readiness"
+            :props="props"
+            class="q-td-readiness"
+          >
+            <q-chip>
+              <div class="text">Готовность: <span>{{props.row.readiness}}</span>%</div>
+            </q-chip>
+            <div class="flex toolbar">
+              <div class="toolbar-procent bg-primary" :style="{width: props.row.readiness + '%'}"></div>
+            </div>
+          </q-td>
+          <q-td
+            key="payment"
+            :props="props"
+            class="q-td-payment"
+          >
+            <q-chip>
+              <div class="text">Оплата: <span>{{props.row.payment}}</span>%</div>
+            </q-chip>
+            <div class="flex toolbar">
+              <div class="toolbar-procent bg-positive" :style="{width: props.row.payment + '%'}"></div>
+            </div>
+          </q-td>
+          <q-td
+            key="timing"
+            :props="props"
+            class="q-td-timing"
+          >
+            <div class="text">Сроки: осталось {{props.row.timing}} дн</div>
+          </q-td>
+          <q-td style="flex: 0 0 100%;order: 7;"></q-td>
+          <q-td
+            key="share"
+            :props="props"
+            class="q-td-share"
+          >
+            <q-list>
+              <q-item
+                v-for="item in props.row.share.slice(0, 3)" :key="item.link"
+              >
+                <q-btn :to="item.link">
+                  <img :src="item.icon" alt="">
+                </q-btn>
               </q-item>
-            </q-td>
-            <q-td
-              key="name"
-              :props="props"
-              class="q-td-name"
-            >
-              <div class="text-h3">
-                <span class="name-ico">{{props.row.iconName}}</span>{{props.row.name}}
-              </div>
-            </q-td>
-            <q-td
-              key="action"
-              :props="props"
-              class="q-td-action"
-            >
-              <ActionBtn 
-                :propsEl="props.row.id"
-                :offsetYX="[55, -258]"
-                :actionfunc="actionfunc"
-                @actionUpdate="onActionUpdate"
-                @actionCopy="onActionCopy"
-                @actionDel="onActionDel"
-              />
-            </q-td>
-            <q-td
-              key="square"
-              :props="props"
-              class="q-td-square"
-            >
-              <div class="text">Квартира, {{props.row.square}} м<sup>2</sup></div>
-            </q-td>
-            <q-td
-              key="changed"
-              :props="props"
-              class="q-td-changed"
-            >
-              <div class="text">Изменен: {{props.row.changed}}</div>
-            </q-td>
-            <q-td
-              key="customer"
-              :props="props"
-              class="q-td-customer"
-            >
-              <div class="text">{{props.row.customer}}</div>
-            </q-td>
-            <q-td
-              key="created"
-              :props="props"
-              class="q-td-created"
-            >
-              <div class="text">Создан: {{props.row.created}}</div>
-            </q-td>
-            <q-td
-              key="address"
-              :props="props"
-              class="q-td-address"
-            >
-              <div class="text">{{props.row.address}}</div>
-            </q-td>
-            <q-td style="flex: 0 0 100%;order: 4;" v-if="tab === 'stripes'"></q-td>
-            <q-td
-              key="readiness"
-              :props="props"
-              class="q-td-readiness"
-            >
-              <q-chip>
-                <div class="text">Готовность: <span>{{props.row.readiness}}</span>%</div>
-              </q-chip>
-              <div class="flex toolbar">
-                <div class="toolbar-procent bg-primary" :style="{width: props.row.readiness + '%'}"></div>
-              </div>
-            </q-td>
-            <q-td
-              key="payment"
-              :props="props"
-              class="q-td-payment"
-            >
-              <q-chip>
-                <div class="text">Оплата: <span>{{props.row.payment}}</span>%</div>
-              </q-chip>
-              <div class="flex toolbar">
-                <div class="toolbar-procent bg-positive" :style="{width: props.row.payment + '%'}"></div>
-              </div>
-            </q-td>
-            <q-td
-              key="timing"
-              :props="props"
-              class="q-td-timing"
-            >
-              <div class="text">Сроки: осталось {{props.row.timing}} дн</div>
-            </q-td>
-            <q-td style="flex: 0 0 100%;order: 7;"></q-td>
-            <q-td
-              key="share"
-              :props="props"
-              class="q-td-share"
-            >
-              <q-list>
-                <q-item
-                  v-for="item in props.row.share.slice(0, 3)" :key="item.link"
-                >
-                  <q-btn :to="item.link">
-                    <img :src="item.icon" alt="">
-                  </q-btn>
-                </q-item>
-                <q-item
-                  v-if="props.row.share.length > 3"
-                >
-                  <q-btn class="q-td-share__btn__limit" :label="`+${props.row.share.length - 3}`" />
-                </q-item>
-                <q-item class="q-item-add">
-                  <q-btn class="q-td-share__btn__add" icon="svguse:icons/allIcons.svg#plus" />
-                </q-item>
-              </q-list>
-            </q-td>
-          </q-tr>
-        </template>
-      </q-table>
+              <q-item
+                v-if="props.row.share.length > 3"
+              >
+                <q-btn class="q-td-share__btn__limit" :label="`+${props.row.share.length - 3}`" />
+              </q-item>
+              <q-item class="q-item-add">
+                <q-btn class="q-td-share__btn__add" icon="svguse:icons/allIcons.svg#plus" />
+              </q-item>
+            </q-list>
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
 
   </q-expansion-item>
   
@@ -231,7 +229,93 @@ export default {
             link: ''
           }
         ]   
-      }
+      },
+      {
+        id: 2123,
+        status: 1,
+        iconName: '💜',
+        name: 'Супер проект',
+        type: 1,
+        typeName: 'Квартиа',
+        address: 'ул пушкина дом калатушкина 5',
+        square: 25,
+        customer: `asdasda`,
+        changed: '25.02.2023',
+        created: '10.02.2023',
+        timing: 50,
+        orderer: 2153,
+        payment: 80,
+        readiness: 40,
+        share: [
+          {
+            icon: '/icons/anton.jpg',
+            link: 's'
+          },
+          {
+            icon: '/icons/stroipro.jpg',
+            link: ''
+          },
+          {
+            icon: '/icons/anton.jpg',
+            link: ''
+          },
+          {
+            icon: '/icons/anton.jpg',
+            link: ''
+          },
+          {
+            icon: '/icons/stroipro.jpg',
+            link: ''
+          },
+          {
+            icon: '/icons/stroipro.jpg',
+            link: ''
+          }
+        ]   
+      },
+      {
+        id: 2123,
+        status: 1,
+        iconName: '💜',
+        name: 'Супер проект',
+        type: 1,
+        typeName: 'Квартиа',
+        address: 'ул пушкина дом калатушкина 5',
+        square: 25,
+        customer: `asdasda`,
+        changed: '25.02.2023',
+        created: '10.02.2023',
+        timing: 50,
+        orderer: 2153,
+        payment: 80,
+        readiness: 40,
+        share: [
+          {
+            icon: '/icons/anton.jpg',
+            link: 's'
+          },
+          {
+            icon: '/icons/stroipro.jpg',
+            link: ''
+          },
+          {
+            icon: '/icons/anton.jpg',
+            link: ''
+          },
+          {
+            icon: '/icons/anton.jpg',
+            link: ''
+          },
+          {
+            icon: '/icons/stroipro.jpg',
+            link: ''
+          },
+          {
+            icon: '/icons/stroipro.jpg',
+            link: ''
+          }
+        ]   
+      },
     ])
 
     return {
